6926ebd18e85730e40018cc058ca1c03df77766a
AnnaAlexandrova1 <any7487@yandex.ru>
2024-02-24 10:41:26 +0300

    роутинг

---
+++ src/app/pages/prompts/prompts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prompts',
+  standalone: true,
+  imports: [],
+  templateUrl: './prompts.component.html',
+  styleUrl: './prompts.component.scss'
+})
+export class PromptsComponent {
+
+}
