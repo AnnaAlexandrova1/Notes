cde42a99b3461d3f25db05fe8e1ba9135fa406a0
AnnaAlexandrova1 <any7487@yandex.ru>
2024-02-29 22:46:27 +0300

    добавлены теги в модалку

--- src/app/pages/notes/components/note-modal/note-modal.component.ts
+++ src/app/pages/notes/components/note-modal/note-modal.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { DividerModule } from 'primeng/divider';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
 
 import { INote } from '../../../../interfaces/note.interface';
 
 @Component({
   selector: 'app-note-modal',
   standalone: true,
-  imports: [DividerModule, InputTextareaModule, ButtonModule, InputTextModule],
+  imports: [DividerModule, InputTextareaModule, ButtonModule, InputTextModule, MultiSelectModule],
   templateUrl: './note-modal.component.html',
   styleUrl: './note-modal.component.scss',
 })
